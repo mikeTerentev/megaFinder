f0bc676db6b67ed1c1ef2102db53d2e7126babb6
mikeTerentev <terentyevmuy@gmail.com>
2018-12-22 16:17:12 +0300

    threads fixed

--- main.cpp
+++ main.cpp
@@ -10,13 +10,7 @@
 #include "tbufferedreader.h"
 #include "QDebug"
 int main(int argc, char *argv[]) {
-    qDebug()<<static_cast<unsigned char>('a');
-    qDebug()<<((static_cast<unsigned char>('a') << 8) + static_cast<unsigned char>('b'));
-    qDebug()<<"abc"<< (((static_cast<unsigned char>('a') << 8) + static_cast<unsigned char>('b')) << 8) + static_cast<unsigned char>('c');
-    TBufferedReader x("/Users/miketerentyev/itmo/poligon.txt");
-    while(x.hasNextTrigram()){
-        qDebug()<<x.hasNextTrigram()<<" "<< x.nextTrigram()<<endl;
-    }
+
     QApplication app(argc, argv);
     main_window w;
     QCoreApplication::setApplicationVersion(QT_VERSION_STR);
